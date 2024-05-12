375a9549b38526dd570cad3d01c7c0fe9ff19055
Pinpacho <julipinchao@gmail.com>
2024-05-12 00:25:22 -0500

    Cintillo de espinas

--- recursividad/dibujo.py
+++ recursividad/dibujo.py
@@ -9,7 +9,7 @@ def spiral(myTurtle,length):
         myTurtle.left(130)
         spiral(myTurtle, length-2)
 
-colors=['#ff0000','#ff8000','#ffff00','#ffff00','#00ff00','#00ff80','#00ffff','#007fff','#0000ff','#8000ff','#ff00ff']
+colors=['#ff0000','#ff8000','#ffff00','#ffff00','#00ff00','#00ff80','#00ffff','#007fff','#0000ff','#8000ff','#ff00ff','#ff0000','#ff8000','#ffff00','#ffff00','#00ff00']
 
 for color in colors:
     myTurtle.pencolor(color)
